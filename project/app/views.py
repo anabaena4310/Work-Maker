045168a15bbc7cce6c3831f84c3da7d218a870eb
松田貴大 <matsudatakashidai@matsudatakashidainoMacBook-Air.local>
2019-10-14 05:03:00 +0900

    add project

---
+++ project/app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def index(request):
+    return render(request, "app/index.html")
+
+def home(request):
+    return render(request, "app/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("app:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'app/signup.html', context)
