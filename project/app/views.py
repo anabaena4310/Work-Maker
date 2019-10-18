cfcdb5ebca0c3bd6a94f2f9cbb5df149b76c94ab
松田貴大 <matsudatakashidai@matsudatakashidainoMacBook-Air.local>
2019-10-18 16:39:15 +0900

    add user management

--- project/app/views.py
+++ project/app/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, DeleteView
 from .form import UserForm
+from .mixins import OnlyYouMixin
 
 # Create your views here.
 
@@ -37,9 +38,16 @@ class UserDetailView(DetailView):
     model = User
     template_name = "app/users/detail.html"
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(OnlyYouMixin, UpdateView):
     model = User
     template_name = "app/users/update.html"
     form_class = UserForm
     def get_success_url(self):
         return resolve_url('app:users_detail', pk=self.kwargs['pk'])
+
+class UserDeleteView(OnlyYouMixin, DeleteView):
+    model = User
+    template_name = "app/users/delete.html"
+
+    def get_success_url(self):
+        return resolve_url('app:index')
